5d3b64cc71ea0acb4252fab334d7d229d57c97a3
medranomatias <mmedrano@beesion.com>
2016-10-13 15:19:16 -0300

    se suben los ultimos cambios

---
+++ Parser/src/frontend/pascal/tokens/PascalErrorToken.java
@@ -0,0 +1,29 @@
+package frontend.pascal.tokens;
+
+import frontend.Source;
+import frontend.pascal.PascalErrorCode;
+
+import static frontend.pascal.PascalTokenType.ERROR;
+
+/**
+ * Created by medranomatias on 13/10/2016.
+ */
+public class PascalErrorToken extends PascalToken{
+    /**
+     * Constructor.
+     *
+     * @param source the source from where to fetch the token's characters.
+     * @throws Exception if an error occurred.
+     */
+    protected PascalErrorToken(Source source, PascalErrorCode errorCode, String tokenText) throws Exception {
+        super(source);
+        this.text = tokenText;
+        this.type = ERROR;
+        this.value = errorCode;
+    }
+
+    @Override
+    protected void extract() throws Exception {
+
+    }
+}
